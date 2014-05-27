677acbfc5b97e8185d7adbc2865c89805bfe366b
Anurag Bhatt <anuragrb@andrew.cmu.edu>
2014-05-27 13:41:36 -0400

    Small bug with reporting interface

--- se/app/reporting/views.py
+++ se/app/reporting/views.py
@@ -41,49 +41,54 @@ def reporting(request):
         time_start = user_profile.begin_time
 
         now = now.replace(tzinfo=None)
-        time_start = time_start.replace(tzinfo=None)
+        if time_start == None:
 
-        difference = now - time_start
-
-        minutes = difference.seconds/60
-
-        user_country = user_profile.country
-
-        if minutes > 30:
-
-            if str(user_profile.end_time) != 'None' or Answer.objects.filter(question_id=68).filter(user=user_profile.user):
-                users_complete += 1
-                if user_country in country_completes.iterkeys():
-                    country_completes[user_country] += 1
-                else:
-                    country_completes[user_country] = 1
-
-            else:
-                user_answers = Answer.objects.filter(user=user_profile.user)
-                user_answers = list(user_answers)
-                if len(user_answers) > 0:
-                    last_answer = user_answers[-1]
-                    last_question = Question.objects.get(answer=last_answer)
-                    qid = str(last_question.id)
-                    if qid in question_terminated.iterkeys():
-                        question_terminated[qid] += 1
-                    else:
-                        question_terminated[qid] = 1
-                else:
-                    if '1' in question_terminated.iterkeys():
-                        question_terminated['1'] += 1
-                    else:
-                        question_terminated['1'] = 1
-                if user_country in country_incompletes.iterkeys():
-                    country_incompletes[user_country] += 1
-                else:
-                    if len(user_country) > 1:
-                        country_incompletes[user_country] = 1
+            country_incompletes += 1
         else:
-            if user_country in country_in_progress.iterkeys():
-                country_in_progress[user_country] += 1
+
+            time_start = time_start.replace(tzinfo=None)
+
+            difference = now - time_start
+
+            minutes = difference.seconds/60
+
+            user_country = user_profile.country
+
+            if minutes > 30:
+
+                if str(user_profile.end_time) != 'None' or Answer.objects.filter(question_id=68).filter(user=user_profile.user):
+                    users_complete += 1
+                    if user_country in country_completes.iterkeys():
+                        country_completes[user_country] += 1
+                    else:
+                        country_completes[user_country] = 1
+
+                else:
+                    user_answers = Answer.objects.filter(user=user_profile.user)
+                    user_answers = list(user_answers)
+                    if len(user_answers) > 0:
+                        last_answer = user_answers[-1]
+                        last_question = Question.objects.get(answer=last_answer)
+                        qid = str(last_question.id)
+                        if qid in question_terminated.iterkeys():
+                            question_terminated[qid] += 1
+                        else:
+                            question_terminated[qid] = 1
+                    else:
+                        if '1' in question_terminated.iterkeys():
+                            question_terminated['1'] += 1
+                        else:
+                            question_terminated['1'] = 1
+                    if user_country in country_incompletes.iterkeys():
+                        country_incompletes[user_country] += 1
+                    else:
+                        if len(user_country) > 1:
+                            country_incompletes[user_country] = 1
             else:
-                country_in_progress[user_country] = 1
+                if user_country in country_in_progress.iterkeys():
+                    country_in_progress[user_country] += 1
+                else:
+                    country_in_progress[user_country] = 1
 
     context['country_in_progress'] = str(json.dumps(country_in_progress))
     context['country_completes'] = str(json.dumps(country_completes))
